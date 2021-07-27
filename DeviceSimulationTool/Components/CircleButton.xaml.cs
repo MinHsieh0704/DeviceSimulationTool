20770db1c07364721b26316309ae4dbd12b01020
Min.Hsieh <Min.Hsieh@advantech.com.tw>
2021-07-27 17:52:38 +0800

    Add CircleButton component

---
+++ DeviceSimulationTool/Components/CircleButton.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DeviceSimulationTool.Components
+{
+    /// <summary>
+    /// CircleButton.xaml 的互動邏輯
+    /// </summary>
+    public partial class CircleButton : Button
+    {
+        #region DependencyProperty
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(CircleButton), new PropertyMetadata(new CornerRadius(30)));
+        #endregion
+
+        public CircleButton()
+        {
+            InitializeComponent();
+        }
+    }
+}

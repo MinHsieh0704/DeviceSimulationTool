d55f586d6cf6caefb04818f5c05614a734c541cc
Min.Hsieh <Min.Hsieh@advantech.com.tw>
2021-09-03 10:33:48 +0800

    Add Boolean Reverse convert

---
+++ DeviceSimulationTool/Converts/BooleanReverseConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DeviceSimulationTool.Converts
+{
+    public class BooleanReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            try
+            {
+                bool flag = System.Convert.ToBoolean(value);
+                if (!flag)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return true;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
